898e3be42d980c2e0d0e869672c0a957e6cb69a0
Lucas Santiago <luccassantiago.contato@gmail.com>
2023-03-28 02:23:00 -0300

    console-title-add

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -24,7 +24,7 @@ public class App {
 
         // Exibir e manipular os dados.
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
+            System.out.println("\u001b[1mTítulo: \u001b[m" + filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
             System.out.println();
