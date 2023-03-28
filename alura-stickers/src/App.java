7954bbff6ddf44c8ba5a97579a6762d0ffde635a
Lucas Santiago <luccassantiago.contato@gmail.com>
2023-03-28 15:36:47 -0300

    title-and-poster-animated

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -24,10 +24,9 @@ public class App {
 
         // Exibir e manipular os dados.
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001b[1mTítulo: \u001b[m" + filme.get("title"));
-            System.out.println(filme.get("image"));
+            System.out.println("\u001b[1m \u001b[34m Título: \u001b[m" + filme.get("title"));
+            System.out.println("\u001b[1m \u001b[34m Poster: \u001b[m" + filme.get("image"));
             System.out.println(filme.get("imDbRating"));
-            System.out.println();
             
         }
     }
